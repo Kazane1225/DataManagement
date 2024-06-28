764ba2eeb8b96c5282ec48926492e594a64e04da
hamao_kazane <hamao_kazane@plise.co.jp>
2024-06-28 11:34:34 +0900

    HOT: initial revision

---
+++ HeadsOrTails.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+public class HeadsOrTails {
+    public static void main(String[] args){
+        Random random = new Random();
+        int count_heads = 0;
+        int count_tails = 0;
+        for(int i = 0; i < 3; i++){
+            System.out.printf("Round %d: ", i + 1);
+            int toss = random.nextInt(2);
+            if (toss == 0){
+                System.out.println("heads");
+                count_heads++;
+            } else {
+                System.out.println("tails");
+                count_tails++;
+            }
+        }
+        System.out.printf("Heads: %d, Tails: %d\n", count_heads, count_tails);
+    }
+}
